3ffa9fa4b8b3591aaa43227a3d7c6995b9699163
Emin Ahmadov <emin.ahmadov99@gmail.com>
2019-10-27 10:48:29 +0100

    Added encryption

--- logic.py
+++ logic.py
@@ -1,37 +1,55 @@
 import ast
 import random
 import requests
+import json
+import sys
+from time import sleep
+from Crypto.Cipher import AES
+from Crypto import Random
+from pkcs7 import PKCS7Encoder
+from base64 import b64encode
 
 
-FIRST_INIT_X = -120.983640
-SECOND_INIT_X = -120.983540
-THIRD_INIT_X = -120.983440
-INIT_Y = 37.077644
 
-# x = format(FIRST_INIT_X, '.4f')
-# x = ast.literal_eval(x)
-# y = format(INIT_Y, '.4f')
-# y = ast.literal_eval(y)
-x = FIRST_INIT_X
-y = INIT_Y
+x = ast.literal_eval(sys.argv[1])
+y = ast.literal_eval(sys.argv[2])
+animal_type = sys.argv[3]
+sensorID = "A4DD5"
+password = 'trackbeeAdmin'
 
-def generate_rand() :
-    rand = random.uniform(0.0001, 0.0009)
+def generate_rand():
+    rand = random.uniform(0.001, 0.009) * (1 if random.random() < 0.5 else -1)
     return rand
 
-url = 'our_domain'
+def encrypt_val(clear_text):
+    master_key = '1234567890123456' 
+    encoder = PKCS7Encoder()
+    raw = encoder.encode(clear_text)
+    iv = Random.new().read( 16 )
+    cipher = AES.new( master_key, AES.MODE_CBC, iv, segment_size=128 )
+    return b64encode( iv + cipher.encrypt( raw ) ).decode('ascii')
+
+url = 'http://localhost:3000/'
+passwordEncrypted = encrypt_val(password)
 
 for i in range(20):
-    print(y, x)
-    x = x + generate_rand()
     y = y + generate_rand()
+    x = x + generate_rand()
+    print(y, x)
     data = {
-        'Latitude': format(x, '.4f'),
-        'Longitude': format(y, '.4f'),
-        'type': 'bee',
-        'key': 'our_key'
+        'latitude': format(y, '.3f'),
+        'longitude': format(x, '.3f'),
+        'type': animal_type,
+        'sensorID': sensorID,
+        'password': passwordEncrypted
     }
-    print(data)
-    # x = requests.post(url, data = data)
-
-
+    headers = {'Content-type': 'application/json'}
+    r = requests.post(url, data = json.dumps(data), headers = headers)
+    waiting_time = 0
+    if animal_type == 'Bee':
+        waiting_time = 2
+    elif animal_type == 'Deer':
+        waiting_time = 1.5
+    elif animal_type == 'Bird':
+        waiting_time = 1
+    sleep(waiting_time)
